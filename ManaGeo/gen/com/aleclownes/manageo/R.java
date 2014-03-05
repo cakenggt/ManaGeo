b461bbca7334c055d64ebb311469a5c16ccbb58a
Alec Lownes <alec.lownes@gmail.com>
2014-03-05 13:18:47 -0500

    Some things in R.java

--- ManaGeo/gen/com/aleclownes/manageo/R.java
+++ ManaGeo/gen/com/aleclownes/manageo/R.java
@@ -24,12 +24,13 @@ public final class R {
     public static final class drawable {
         public static final int direction=0x7f020000;
         public static final int forest=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int mine=0x7f020003;
-        public static final int no_gps=0x7f020004;
-        public static final int plain=0x7f020005;
-        public static final int refinery=0x7f020006;
-        public static final int warehouse=0x7f020007;
+        public static final int house=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int mine=0x7f020004;
+        public static final int no_gps=0x7f020005;
+        public static final int plain=0x7f020006;
+        public static final int refinery=0x7f020007;
+        public static final int warehouse=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f080029;
